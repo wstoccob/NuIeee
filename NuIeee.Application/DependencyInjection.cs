9882205e4347facb1e5adf810bd04a2e177aabef
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-04 16:37:54 +0500

    Add MediatR

---
+++ NuIeee.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NuIeee.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+        
+        return services;
+    }
+}
