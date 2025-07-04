ab6f9a6029cfa2be963f755791467612a1e98f65
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-04 16:38:38 +0500

    Add Logging

---
+++ NuIeee.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NuIeee.Domain.Entities;
+using NuIeee.Infrastructure.Persistence;
+
+namespace NuIeee.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(config.GetConnectionString("DefaultConnection")));
+
+        services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+            {
+                options.User.RequireUniqueEmail = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+}
