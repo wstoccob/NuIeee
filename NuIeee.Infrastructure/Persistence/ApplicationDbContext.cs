372982bb7c758aff72842736f7873bc388459951
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-04 16:37:43 +0500

    Add ApplicationUser

---
+++ NuIeee.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NuIeee.Domain.Entities;
+
+namespace NuIeee.Infrastructure.Persistence;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
