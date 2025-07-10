9af32f1b46b11a05278c0d076977e42639c09635
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-10 13:11:25 +0500

    Change method arguments' count

--- NuIeee.Application/Interfaces/IUserManagementRepository.cs
+++ NuIeee.Application/Interfaces/IUserManagementRepository.cs
@@ -6,7 +6,7 @@ namespace NuIeee.Application.Interfaces;
 public interface IUserManagementRepository
 {
     Task<List<UserDto>> GetAllUsersAsync();
-    Task<Guid> CreateUserAsync(string username, string password, string role);
+    Task<Guid> CreateUserAsync(string username, string fullname, string password, string role);
     Task<bool> DeleteUserAsync(Guid userId);
     Task<UserDto?> GetUserByIdAsync(Guid userId);
 }
