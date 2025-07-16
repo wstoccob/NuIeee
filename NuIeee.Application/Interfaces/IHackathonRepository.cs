ebbaf0743215a17059e0e12a03c8b1b017c990f5
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-16 16:54:55 +0500

    Create HackathonRepository.cs

---
+++ NuIeee.Application/Interfaces/IHackathonRepository.cs
@@ -0,0 +1,8 @@
+﻿using NuIeee.Domain.Entities;
+
+namespace NuIeee.Application.Interfaces;
+
+public interface IHackathonRepository
+{
+    Task AddTeamAsync(Team team, CancellationToken cancellationToken);
+}
---
+++ NuIeee.Infrastructure/Repositories/HackathonRepository.cs
@@ -0,0 +1,20 @@
+﻿using NuIeee.Domain.Entities;
+using NuIeee.Infrastructure.Persistence;
+
+namespace NuIeee.Infrastructure.Repositories;
+
+public class HackathonRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public HackathonRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddTeamAsync(Team team, CancellationToken cancellationToken)
+    {
+        await _context.Teams.AddAsync(team, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
