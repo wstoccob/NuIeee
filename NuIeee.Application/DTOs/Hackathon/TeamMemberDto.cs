44cf7427f682eafb8449e5f95916548e04535553
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-16 17:26:59 +0500

    Add TeamMemberDto.cs

---
+++ NuIeee.Application/DTOs/Hackathon/TeamMemberDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NuIeee.Application.DTOs.Hackathon;
+
+public class TeamMemberDto
+{
+    public string FullName { get; set; }
+    public string NuId { get; set; }
+    public string? Iin { get; set; }
+    public string Email { get; set; }
+    public string YearOfStudy { get; set; }
+    public string Major { get; set; }
+}
