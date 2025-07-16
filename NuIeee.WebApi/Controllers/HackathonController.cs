98191d172874c5c54eb0588b9194f578a0e1ab2b
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-16 17:27:45 +0500

    Add HackathonController.cs

---
+++ NuIeee.WebApi/Controllers/HackathonController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NuIeee.Application.Features.Hackathon.Commands;
+
+namespace NuIeee.WebApi.Controllers;
+
+[ApiController]
+[Route("api/hackathon")]
+public class HackathonController(IMediator _mediator) : ControllerBase
+{
+    [HttpPost("register-team")]
+    public async Task<IActionResult> RegisterTeamAsync(RegisterTeamCommand command)
+    {
+        await _mediator.Send(command);
+        
+        return Ok();
+    }
+}
