f2eb0be4216c52a2fc7d2af6b09a6e89dec4bfd9
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-16 17:27:13 +0500

    Add RegisterTeamCommand.csr

---
+++ NuIeee.Application/Features/Hackathon/Commands/RegisterTeamCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using NuIeee.Application.DTOs.Hackathon;
+
+namespace NuIeee.Application.Features.Hackathon.Commands;
+
+public record RegisterTeamCommand(string TeamName, List<TeamMemberDto> Members) : IRequest;
