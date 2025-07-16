79d60f135398de75fd2f291dbb3e89408fbc8e7d
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-16 17:27:35 +0500

    Add RegisterTeamCommandHandler.cs

---
+++ NuIeee.Application/Features/Hackathon/Handlers/RegisterTeamCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using NuIeee.Application.Features.Hackathon.Commands;
+using NuIeee.Application.Interfaces;
+using NuIeee.Domain.Entities;
+
+namespace NuIeee.Application.Features.Hackathon.Handlers;
+
+public class RegisterTeamCommandHandler : IRequestHandler<RegisterTeamCommand>
+{
+    private readonly IHackathonRepository _hackathonRepository;
+
+    public RegisterTeamCommandHandler(IHackathonRepository  hackathonRepository)
+    {
+        _hackathonRepository = hackathonRepository;
+    }
+    public async Task Handle(RegisterTeamCommand request, CancellationToken cancellationToken)
+    {
+        var team = new Team
+        {
+            Name = request.TeamName,
+            Members = request.Members.Select((dto, index) => new TeamMember
+            {
+                FullName = dto.FullName,
+                NuId = dto.NuId,
+                Iin = dto.Iin,
+                Email = dto.Email,
+                YearOfStudy = dto.YearOfStudy,
+                Major = dto.Major,
+                IsCaptain = index == 0 // First member is the captain
+            }).ToList()
+        };
+
+        await _hackathonRepository.AddTeamAsync(team, cancellationToken);
+    }
+}
