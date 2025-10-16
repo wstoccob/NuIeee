7365a15e0a007e2fe13dc21d847452e093727a9c
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-10-16 22:25:24 +0500

    Create EventsController

---
+++ NuIeee.WebApi/Controllers/EventsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NuIeee.Application.Features.Events.Commands;
+
+namespace NuIeee.WebApi.Controllers;
+
+[ApiController]
+[Route("api/events")]
+public class EventsController(IMediator mediator) : ControllerBase
+{
+    /*[HttpGet]
+    public async Task<IActionResult> GetAllEvents()
+    {
+        var result = await mediator.Send();
+        return Ok(result);
+    }*/
+    
+    [Authorize(Roles="SuperAdmin")]
+    [HttpPost("create-event")]
+    public async Task<IActionResult> CreateEventAsync(CreateEventCommand command)
+    {
+        var id = await mediator.Send(command);
+        return Ok(id);
+    }
+}
