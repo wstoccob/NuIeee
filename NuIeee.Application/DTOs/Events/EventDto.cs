2ea1ba5b75e9c35388670909b64de94d3b0cb4d0
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-10-16 22:25:12 +0500

    Add CreateEventCommand and CreateEventCommandHandler

---
+++ NuIeee.Application/DTOs/Events/EventDto.cs
@@ -0,0 +1,12 @@
+﻿namespace NuIeee.Application.DTOs.Events;
+
+public class EventDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime EventDateTime { get; set; }
+    public bool HasRegistrationLink { get; set; }
+    public Uri? RegistrationLink { get; set; }
+
+    private List<EventPhotoDto> Photos { get; set; }
+}
---
+++ NuIeee.Application/DTOs/Events/EventPhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace NuIeee.Application.DTOs.Events;
+
+public class EventPhotoDto
+{
+    public string AlternativeText { get; set; }
+    public Uri PhotoLink { get; set; }
+}
