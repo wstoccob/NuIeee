5af948f5bdb314abbdf2de12943569077671534a
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-10-16 22:24:19 +0500

    Add EventRepository.cs

---
+++ NuIeee.Application/Interfaces/IEventRepository.cs
@@ -0,0 +1,8 @@
+﻿using EventEntity = NuIeee.Domain.Entities.Event;
+
+namespace NuIeee.Application.Interfaces;
+
+public interface IEventRepository
+{
+    Task<Guid> AddEventAsync(EventEntity eventEntity, CancellationToken cancellationToken);
+}
---
+++ NuIeee.Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,17 @@
+﻿using NuIeee.Application.Interfaces;
+using NuIeee.Domain.Entities;
+using NuIeee.Infrastructure.Persistence;
+using EventEntity = NuIeee.Domain.Entities.Event;
+
+namespace NuIeee.Infrastructure.Repositories;
+
+public class EventRepository(ApplicationDbContext _context) : IEventRepository
+{
+    public async Task<Guid> AddEventAsync(EventEntity eventEntity, CancellationToken cancellationToken)
+    {
+        await _context.Events.AddAsync(eventEntity, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return eventEntity.Id;
+    }
+}
